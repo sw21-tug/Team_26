894b18bfb10d96dd436dc7901807539741d139ee
Tania Kaimel <tania.kaimel@student.tugraz.at>
2021-05-02 18:54:15 +0200

    [TL-8] Added navigation drawer menu for groups (KT, GR)

---
+++ Chronos/app/src/main/java/com/tugraz/chronos/CreateGroupActivity.kt
@@ -0,0 +1,37 @@
+package com.tugraz.chronos
+
+import android.app.Notification
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+
+class CreateGroupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_group)
+        val drawer: DrawerLayout = findViewById(R.id.drawer_layout)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open,
+        R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        // Enables Always-on
+        // setAmbientEnabled()
+    }
+
+    override fun onBackPressed() {
+        val drawer: DrawerLayout = findViewById(R.id.drawer_layout)
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
