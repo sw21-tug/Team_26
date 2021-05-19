507c47a93fbb2bb271119aad00a6bfb9cc037ddd
Amir Mujacic <amir.mujacic@hotmail.com>
2021-05-19 14:34:06 +0200

    [TL 7] refactoring scrollview to recyclerview (FM, MA)

--- Chronos/app/src/main/java/com/tugraz/chronos/MainActivity.kt
+++ Chronos/app/src/main/java/com/tugraz/chronos/MainActivity.kt
@@ -1,25 +1,24 @@
 package com.tugraz.chronos
 
-import android.content.ClipData
 import android.content.Intent
-import android.graphics.Color
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.LayoutInflater
 import android.view.MenuItem
-import android.view.View
-import android.widget.Button
-import android.widget.LinearLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 import com.tugraz.chronos.model.entities.Task
 import com.tugraz.chronos.model.service.ChronosService
@@ -30,9 +29,59 @@ import java.util.*
 
 lateinit var chronosService: ChronosService
 
+class TaskItemHolder(inflater: LayoutInflater, parent: ViewGroup) :
+        RecyclerView.ViewHolder(inflater.inflate(R.layout.task_recycler_item, parent, false)) {
+    private var mTitle: TextView? = null
+    private var mDate: TextView? = null
+
+
+    init {
+        mTitle = itemView.findViewById(R.id.tv_tri_title)
+        mDate = itemView.findViewById(R.id.tv_tri_date)
+    }
+
+    fun bind(task: Task) {
+        val title = task.title
+        val date1 = LocalDateTime.parse(
+                task.date,
+                DateTimeFormatter.ISO_DATE_TIME
+        )
+        val date2 = LocalDateTime.now()
+
+        val input: Long = date2.until(date1, ChronoUnit.SECONDS)
+
+        val days = input / 86400
+        val hours = (input % 86400 ) / 3600
+        val minutes = ((input % 86400 ) % 3600 ) / 60
+        val seconds = ((input % 86400 ) % 3600 ) % 60
+        val timeUntil = days.toString() + "d " + hours.toString() + ":" + minutes.toString() + ":" + seconds.toString()
+
+        mTitle?.text = title
+        mDate?.text = timeUntil
+    }
+}
+
+
+class ListAdapter(private var list: List<Task>)
+    : RecyclerView.Adapter<TaskItemHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskItemHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return TaskItemHolder(inflater, parent)
+    }
+
+    override fun onBindViewHolder(holder: TaskItemHolder, position: Int) {
+        val movie: Task = list[position]
+        holder.bind(movie)
+    }
+
+    override fun getItemCount(): Int = list.size
+}
+
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     lateinit var swipeRefreshLayout: SwipeRefreshLayout
+    lateinit var list_recycler_view: RecyclerView
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,78 +93,38 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         swipeRefreshLayout = findViewById(R.id.srl_ma)
         swipeRefreshLayout.setOnRefreshListener {
-            loadTasks()
+            val task_list = sortTasks(chronosService.getAllTasks())
+            list_recycler_view.adapter = ListAdapter(task_list)
             Handler(Looper.getMainLooper()).postDelayed({
                 swipeRefreshLayout.isRefreshing = false
             }, 500)
         }
 
-        loadTasks()
+        val task_list = sortTasks(chronosService.getAllTasks())
+        list_recycler_view = findViewById(R.id.rv_ma)
+        list_recycler_view.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = ListAdapter(task_list)
+        }
+
         val fab = findViewById<FloatingActionButton>(R.id.btn_ma_add)
         fab.setOnClickListener {
             startActivity(Intent(this, CreateTaskActivity::class.java))
             finish()
         }
-
-
     }
 
     fun sortTasks(task_list: List<Task>): List<Task> {
         return task_list.sortedBy { value -> value.date }
     }
 
-    fun loadTasks() {
-        val task_list = sortTasks(chronosService.getAllTasks())
-        val linearLayout: LinearLayout = findViewById(R.id.ll_ma_sv)
-        linearLayout.removeAllViews()
-        var item_counter = 0
-        for (item in task_list) {
-
-            val button = Button(this)
-            button.layoutParams =
-                LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 220)
-            button.id = item.taskId.toInt()
-
-            val title = item.title
-            val description = item.description
-            val date1 = LocalDateTime.parse(
-                item.date,
-                DateTimeFormatter.ISO_DATE_TIME
-            )
-            val date2 = LocalDateTime.now()
-
-            val input: Long = date2.until(date1, ChronoUnit.SECONDS)
-
-            val days = input / 86400
-            val hours = (input % 86400 ) / 3600
-            val minutes = ((input % 86400 ) % 3600 ) / 60
-            val seconds = ((input % 86400 ) % 3600 ) % 60
-
-            val space = "    "
-            val timeUntil = days.toString() + "d " + hours.toString() + ":" + minutes.toString() + ":" + seconds.toString()
-            val text = title + space + description + "\n" + timeUntil
-
-            button.text = text
-            button.setTextColor(Color.BLACK)
-            button.textSize = 15F
-
-            if (item_counter % 2 == 0) {
-                button.setBackgroundColor(Color.LTGRAY)
-            } else {
-                button.setBackgroundColor(Color.GRAY)
-            }
-
-            item_counter += 1
-            linearLayout.addView(button)
-        }
-    }
     private fun initNavigationDrawer() {
         val drawer: DrawerLayout = findViewById(R.id.drawer_layout)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
         val toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close)
+                R.string.navigation_drawer_close)
         drawer.addDrawerListener(toggle)
         toggle.syncState()
 
